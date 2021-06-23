9e38ad67d332b8b89a2b7a63cf7027888990985b
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-23 18:52:29 +0530

    code before mongodb and basic express api routes

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('app is listening on port ', port);
 });
+
+//code before mongodb and code of basic express api section 6
