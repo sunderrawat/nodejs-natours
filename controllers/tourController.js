dbc9c19e0d559540cb62764781d3787ec13763ae
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-24 00:28:07 +0530

    reading document get all tours and one tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,39 @@
 const Tour = require('./../model/tourModel');
 
 //Route handelr functions
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // result: tours.length,
-    requestTime: req.requestTime,
-    data: {
-      // tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      requestTime: req.requestTime,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  //   console.log(req.params);
-  res.status(200).json({
-    status: 'success',
-    data: { tour: findOneTour },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+ 
 };
 
 exports.updateTour = (req, res) => {
