3cbd4637dfa95d417faa977fa63d196fbb6333bc
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-23 14:38:29 +0530

    adding a new middelware for practice

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+//middelware
+// global middelware
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('hello from middelware..');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.requestTime);
+  next();
+});
 
 const text = 'hello write file from txt';
 fs.writeFileSync(`${__dirname}/dev-data/data/newfile.txt`, text);
@@ -11,10 +25,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//Route handelr functions
+
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     result: tours.length,
+    requestTime: req.requestTime,
     data: {
       tours,
     },
@@ -87,6 +104,8 @@ const createTour = (req, res) => {
   );
 };
 
+//Routes
+
 // app.post('/api/v1/tours', createTour);
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
@@ -94,6 +113,10 @@ const createTour = (req, res) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 module.exports = app;
