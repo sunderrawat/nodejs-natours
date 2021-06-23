560bfdad6b113447e36e2e337efc4cf0063c9da5
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-23 22:47:16 +0530

    create a tour schema and model from mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-dotenv.config({path: './config.env'})
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // console.log(app.get('env')); // get a running enviornment type
@@ -19,6 +19,26 @@ mongoose
     // console.log(con.connections);
     console.log('DB connected sucessfully...');
   });
+
+//create a tour schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: ['true', 'A tour must have a name'],
+    unique: ['true', 'A tour name is unique'],
+  },
+  price: {
+    type: Number,
+    required: ['true', 'A tour must be a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+//define a tour model
+const Tour = mongoose.model('Tour', tourSchema);
 
 app.listen(port, () => {
   console.log('app is listening on port ', port);
