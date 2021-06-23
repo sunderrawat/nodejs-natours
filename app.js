13205dc13cc2b8eb1d2643017bb183e5971292d3
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-23 11:17:29 +0530

    adding dev-data files and handle get tours route file base api

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+const fs = require('fs');
 const express = require('express');
+const { json } = require('express');
+const { parse } = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello from server-side');
-  //   res.status(200).json({ message: 'hello from server side', app: 'Natours' });  // send data in json format
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('hello from server-side');
+//   //   res.status(200).json({ message: 'hello from server side', app: 'Natours' });  // send data in json format
+// });
 
-app.post('/', (req, res) => {
-  //   res.send('you can post on this route');
-  res.status(200).json({ message: 'post data on this route', app: 'natours' });
-});
+// app.post('/', (req, res) => {
+//   //   res.send('you can post on this route');
+//   res.status(200).json({ message: 'post data on this route', app: 'natours' });
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
 
 module.exports = app;
