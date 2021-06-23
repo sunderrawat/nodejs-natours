b2c7ea61fca36f222561c4f44bc217b80bf1f598
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-23 12:34:55 +0530

    make a file based post request for create new tour

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { json } = require('express');
 const { parse } = require('path');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).send('hello from server-side');
@@ -15,17 +16,50 @@ const app = express();
 //   res.status(200).json({ message: 'post data on this route', app: 'natours' });
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const text = 'hello write file from txt';
+fs.writeFileSync(`${__dirname}/dev-data/data/newfile.txt`, text);
 
-app.get('/api/v1/tours', (req, res)=>{
-    res.status(200).json({
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   const addTour = tours.push(req.body);
+  //   fs.writeFileSync(
+  //     `${__dirname}/dev-data/data/tours-simple.json`,
+  //     JSON.stringify(tours)
+  //   );
+
+  //   res.status(201).json({
+  //     status: 'success',
+  //     result: tours.length,
+  //     data: {
+  //       tours,
+  //     },
+  //   });
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
         status: 'success',
-        result: tours.length,
-        data: {
-            tours
-        }
-    })
-})
-
+        data: newTour,
+      });
+    }
+  );
+});
 
 module.exports = app;
