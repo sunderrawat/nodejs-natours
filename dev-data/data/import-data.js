dfac4360d8bcb927b615df16857a58b43b4d0b13
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-24 11:42:56 +0530

    write script for importing and deleting data

---
+++ dev-data/data/import-data.js
@@ -0,0 +1,47 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+const Tour = require('./../../model/tourModel');
+const fs = require('fs');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connected sucessfully...');
+  });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfuly loaded..');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfuly deleted..');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- model/tourModel.js
+++ model/tourModel.js
@@ -5,14 +5,39 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: ['true', 'A tour must have a name'],
     unique: ['true', 'A tour name is unique'],
+    trim: true,
   },
   price: {
     type: Number,
     required: ['true', 'A tour must be a price'],
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: Number,
+  maxGroupSize: Number,
+  duration: Number,
+  difficulty: String,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image cover'],
+  },
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
