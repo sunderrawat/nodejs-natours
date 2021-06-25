d4985b1acf1be610a74f3689e238b296557b0313
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-25 20:11:38 +0530

    handling unhandeled routes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -9,27 +8,24 @@ const app = express();
 
 //middelware
 // global middelware
-if ((process.env.NODE_ENV === 'development')) {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('hello from middelware..');
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
-  next();
-});
 
 //Routes
 //mounting routes
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//handling unhandeled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find this ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
