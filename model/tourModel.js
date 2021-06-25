d171246c3b290010233500f0ef03eb72f5881a15
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-25 13:02:07 +0530

    use durationWeek virtual property

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,45 +1,56 @@
 const mongoose = require('mongoose');
 //create a tour schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: ['true', 'A tour must have a name'],
-    unique: ['true', 'A tour name is unique'],
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: ['true', 'A tour must have a name'],
+      unique: ['true', 'A tour name is unique'],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: ['true', 'A tour must be a price'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: Number,
+    maxGroupSize: Number,
+    duration: Number,
+    difficulty: String,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a image cover'],
+    },
+    images: [String],
+    startDates: [Date],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
   },
-  price: {
-    type: Number,
-    required: ['true', 'A tour must be a price'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: Number,
-  maxGroupSize: Number,
-  duration: Number,
-  difficulty: String,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a image cover'],
-  },
-  images: [String],
-  startDates: [Date],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//virtuals
+tourSchema.virtual('durationWeek').get(function () {
+  return this.duration / 7;
 });
 
 //define a tour model
