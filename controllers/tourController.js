f228ae80995ae10ed647d504cedb8635be0a8be2
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-26 16:12:44 +0530

    adding 404 not found error and spell fix statusCode in AppError class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const { exists } = require('./../model/tourModel');
 const Tour = require('./../model/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5';
@@ -33,6 +34,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+  if (!tour) {
+    return next(new AppError('no tour find with this id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: { tour },
@@ -45,6 +49,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!tour) {
+    return next(new AppError('no tour find with this id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: { tour },
@@ -52,7 +59,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('no tour find with this id', 404));
+  }
   res.status(204).json({
     status: 'success',
     message: 'tour deleted',
--- utils/appError.js
+++ utils/appError.js
@@ -1,9 +1,9 @@
 class AppError extends Error {
-  constructor(message, stausCode) {
+  constructor(message, statusCode) {
     super(message);
 
-    this.stausCode = stausCode;
-    this.status = `${stausCode}`.startsWith('4') ? 'fail' : 'error';
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
