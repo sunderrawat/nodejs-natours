1a5dc4dc967025f3e5aa1270c80f40dec7eb151d
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-26 18:24:07 +0530

    genrate different error for development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,34 @@
+const sendProdError = (err, res) => {
+  if (!err.isOperational) {
+    console.error('ERROR 💥 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!! 😧',
+    });
+  }
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const sendDevError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdError(err, res);
+  }
 };
