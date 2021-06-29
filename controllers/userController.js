d97c4e092559bc87c99bf49a46c9fbeaf24d5d8b
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-29 19:10:14 +0530

    updating the current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,16 @@
+const User = require('./../model/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
 //users route handeler
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
@@ -23,6 +36,31 @@ exports.updateUser = (req, res) => {
     message: 'route not yet defined',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1 create error if user post password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  //2 update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    user: updatedUser,
+  });
+});
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'fail',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,12 @@ router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:resetToken').patch(authController.resetPassword);
 
-router.route('/updateMyPassword').patch(authController.protect, authController.updatePassword);
+router
+  .route('/updateMyPassword')
+  .patch(authController.protect, authController.updatePassword);
+router
+  .route('/updateMe')
+  .patch(authController.protect, userController.updateMe);
 
 //user routes
 router
