f9625cdc53748a271cf45d27dcab3d69b321ce82
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-29 22:48:20 +0530

    creating cookie and send to response

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,16 @@ const genrateToken = (id) => {
 const createSendTokenRes = (user, statusCode, res) => {
   const token = genrateToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  //create cookie and send to browser
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     statusCode,
