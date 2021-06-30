846f119cb90c58181a74bd63271da25c1edf4665
Sunder Rawat <sunderrawat777@gmail.com>
2021-06-30 18:09:44 +0530

    add header and data sanitization and complete authentication authorization and security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,11 +15,50 @@ const app = express();
 
 //middelware
 // global middelware
+
+// set security http header
+app.use(helmet());
+
+//set node enviorment
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.static(`${__dirname}/public`));
+
+//set request limit
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100,
+  message:
+    'Too many accounts created from this IP, please try again after an hour',
+});
+
+app.use('/api', apiLimiter);
+
+//use body parser for reading body data
 app.use(express.json());
+
+//serving static files
+app.use(express.static(`${__dirname}/public`));
+
+//Data sanitize against NoSQL query
+app.use(mongoSanitize());
+
+//Data sanitize against xss
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'price',
+      'difficulty',
+    ],
+  })
+);
 
 //Routes
 //mounting routes
