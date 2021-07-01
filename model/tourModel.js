e2562d29142b93027092b45ef2aff45c893c5507
Sunder Rawat <sunderrawat777@gmail.com>
2021-07-01 09:09:40 +0530

    modeling locations (Geospatial data) added in tour model

--- model/tourModel.js
+++ model/tourModel.js
@@ -81,6 +81,29 @@ const tourSchema = new mongoose.Schema(
     slug: {
       type: String,
     },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
