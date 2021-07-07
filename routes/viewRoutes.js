346f963567a86b6b136543787c933ef30579be7f
Sunder Rawat <sunderrawat777@gmail.com>
2021-07-07 09:38:32 +0530

    add login form screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 
+router.get('/login', viewsController.getLogin);
+
 router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
