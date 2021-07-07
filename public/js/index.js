ed7dde6067bd58099dcd573c346ab808ef314b03
Sunder Rawat <sunderrawat777@gmail.com>
2021-07-07 17:06:43 +0530

    add login functionaliy

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,20 @@
+import '@babel/polyfill';
 import { displayMap } from './mapbox';
+import { login } from './login';
+import 'regenerator-runtime/runtime.js';
 
-const mapId = document.getElementById('map');
-if (mapId) {
-  const locations = JSON.parse(mapId.dataset.locations);
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
   displayMap(locations);
 }
+
+if (loginForm)
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', viewsController.getOverview);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-router.get('/login', viewsController.getLogin);
+router.get('/login', authController.isLoggedIn, viewsController.getLogin);
 
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
 module.exports = router;
