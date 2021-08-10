2ea21721216c332ec788380b1c4fd28c73937780
Sunder Rawat <sunderrawat777@gmail.com>
2021-08-10 16:31:41 +0530

    add something

--- public/js/login.js
+++ public/js/login.js
@@ -19,7 +19,8 @@ export const login = async (email, password) => {
       }, 1500);
     }
   } catch (err) {
-    showAlert('error', err.response.data.message);
+    showAlert('error', err);
+    // console.log(err);
   }
 };
 
